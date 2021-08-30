d53138d1c40de48070152955f5ba2509fb809da2
srpadilla <srpadilla@misena.edu.co>
2021-08-30 15:29:39 -0500

    Avanzamos a funciones con arreglos como parámetro

---
+++ arreglos.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    function sumaVector($vector){
+        $sum=0;
+        for ($i=0; $i <count($vector) ; $i++) { 
+                $sum+=$vector[$i];
+            }
+        return $sum; 
+    }
+
+    $vec=array();
+
+    $vec2=[34,56,123,87,1];
+
+    for ($i=0; $i < 10 ; $i++) { 
+        $vec[$i]=rand(0,100);
+    }
+
+    for ($i=0; $i <count($vec) ; $i++) { 
+        echo $vec[$i].', ';
+    }
+
+    echo '<br> la suma de vec2 es= <br>'.sumaVector($vec2);
+    echo '<br> la suma de vec es= <br>'.sumaVector($vec);
+
+
+
+    // $sum=0;
+// for ($i=0; $i <count($vec) ; $i++) { 
+//         $sum+=$vec[$i];
+//     }
+// echo '<br> la suma es= <br>'.$sum;
+// $sum2=0;
+// for ($i=0; $i <count($vec2) ; $i++) { 
+//     $sum2+=$vec2[$i];
+// }
+// echo '<br> la suma es= <br>'.$sum2;
+
+    ?>
+</body>
+</html>
--- inicio.php
+++ inicio.php
@@ -9,6 +9,30 @@
 <body>
     <?php 
     echo 'Comenzamos el desarrollo de software gestionado con GIT y Github';
-     ?>
+    $arreglo=array();
+    $arreglo[0]=100;
+    $arreglo[1]='sena';
+    $arreglo[2]=3.5;
+    $arreglo2=['uno',2,3.0,'cuatro'];
+echo '<br>';
+echo 'Tamaño del arreglo con función count:'.count($arreglo);
+echo '<br>';
+for ($i=0; $i < count($arreglo) ; $i++) { 
+    echo $arreglo[$i].'<br>';
+}
+echo 'ciclo for <br>';
+for ($i=0; $i < count($arreglo2) ; $i++) { 
+    echo $arreglo2[$i].'<br>';
+}
+
+$j=0;
+
+echo 'ciclo while <br>';
+while ($j < count($arreglo)) {    
+    echo $arreglo[$j].'<br>';
+    $j++;
+}
+
+?>
 </body>
 </html>
