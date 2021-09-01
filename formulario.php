ab665c5bcd98c0b3da2fd38f220b62c3bc4031e1
srpadilla <srpadilla@misena.edu.co>
2021-09-01 17:40:20 -0500

    Explicación metodología BEM para CSS

---
+++ formulario.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
+    <title>Document</title>
+</head>
+<body>
+    <header class="header">
+        <h1 class="header__h1">METODOLOGIA BEM PARA CSS</h1>
+    </header>
+    <main class="main">
+            <p class="main__p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium sequi eos repudiandae explicabo nemo, voluptates libero soluta inventore consequuntur voluptas facilis eius quae iure, eveniet similique dolorum sit sed doloremque?</p>
+            <p class="main__p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis illum corrupti libero amet odit in, maxime similique hic. Aut quas neque, expedita eveniet ratione amet laboriosam quam autem praesentium architecto.</p>
+            <p class="main__p main__p--resaltar">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe labore dicta nihil exercitationem consequuntur optio voluptate, quidem ad mollitia fugiat perferendis beatae eius quo id voluptatum sequi illo omnis! In!</p>
+    </main>
+</body>
+</html>
